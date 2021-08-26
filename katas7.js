19ac5cc122a6f364ef1df67d34ba66db1783273c
Larissa Oliveira <oliveir5uwm@gmail.com>
2021-08-26 16:07:51 -0300

    feat:terminadas foreach, fill, map

---
+++ katas7.js
@@ -0,0 +1,67 @@
+// /*forEach*/
+// let array = [1, 2, 3, 4, 5, 6]
+// function newForEach(array, callback){
+//     for(let i = 0; i < array.length; i++){
+//         callback(array[i], i, array)
+//     }
+// }
+// function mostrarElementosForEach(atual, index, arr){
+//     console.log(atual, index, arr)
+// }
+
+// newForEach(array, mostrarElementosForEach)
+
+
+// /*fill*/
+// let array1 = [7, 8, 9, 10, 11, 12]
+
+// function newFill(array, valor, inicio = 0, fim = array.length){
+//     for(let i = inicio; i < fim; i++){
+//     array[i] = valor
+//  }    
+//  return array
+// }
+
+// console.log(newFill(array1, 1, 2, 4))
+
+
+/*map*/
+// let array2 = [13, 14, 15, 16, 17, 18]
+
+// function newMap(array, callback){
+//     let newArr = []
+//     for(let i = 0; i < array.length; i++){
+//         newArr.push(callback(array[i], i, array))
+//     }
+//     return newArr
+// }
+
+// function mostrarElementosMap (atual, index, arr){
+//     return atual*2
+// }
+
+// console.log(newMap(array2, mostrarElementosMap))
+
+
+/*some*/
+let array3 = [14, 15, 16, 17, 18, 19]
+ callback(atual, index, arr)
+function newSome(){
+
+}
+
+
+/*find*/
+/*findIndex*/
+/*every*/
+/*filter*/
+/*concat*/
+/*includes*/
+/*indexOf*/
+/*join*/
+/*reduce*/
+/*slice*/
+/*flat*/
+/*flatMap*/
+/*Array.of*/
+// newForEach, newFill, newMap, newSome, newFind, newFindIndex, newEvery, newFilter, newConcat, newIncludes, newIndexOf, newJoin, newReduce, newSlice, newFlat, newFlatMap e newArrayOf
